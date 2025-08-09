b4bb25247a1a09a54e19d28100e24d62787f14ff
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-08-10 03:45:17 +0530

    Enhance AdminRepository: implement student addition with auto-generated ID and password hashing; add class stream; delete class counter; refactor student stream method

--- admin/lib/repositories/admin_repository.dart
+++ admin/lib/repositories/admin_repository.dart
@@ -1,5 +1,6 @@
 import 'package:ama_meet_admin/models/classroom.dart';
 import 'package:ama_meet_admin/models/student.dart';
+import 'package:ama_meet_admin/utils/hash.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -33,17 +34,46 @@ class AdminRepository {
       batch.delete(doc.reference);
     }
     await batch.commit();
+
+    //Delete counter
+    await _firestore.collection('counters').doc(cId).delete();
+  }
+
+  Stream<List<ClassRoom>> classStream() {
+    return _firestore.collection('classes').snapshots().map((snap) =>
+      snap.docs.map((doc) => ClassRoom.fromMap(doc.data())).toList());
   }
 
   //Students
-  Future<void> addStudent(Student st) async{
-    //auto Id for now
-    await _firestore.collection('students').add(st.toMap());
+  // Future<void> addStudent(Student st) async{
+  //   //auto Id for now
+  //   await _firestore.collection('students').add(st.toMap());
+  // }
+
+  // Add students with auto gen ID
+  Future<void> addStudent({
+    required String classId,
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    final studentId = await _generateStudentId(classId);
+    final passwordHash = sha256Hash(password);
+    final student = Student(
+      id: studentId,
+      classId: classId,
+      name: name,
+      email: email,
+      passwordHash: passwordHash,
+      createdAt: DateTime.now().millisecondsSinceEpoch
+    );
+
+    await _firestore.collection('students').add(student.toMap());
   }
 
-  Stream<List<Student>> studentStreamForClass(String cId) {
+  Stream<List<Student>> studentStreamForClass(String classId) {
     return _firestore.collection('students')
-      .where('classId', isEqualTo: cId)
+      .where('classId', isEqualTo: classId)
       .snapshots()
       .map((snap) => snap.docs.map((doc) => Student.fromMap(doc.data())).toList());
   }
@@ -51,5 +81,20 @@ class AdminRepository {
   // Delete using doc id for now
   Future<void> deleteStudentByDocId(String docId) async {
     await _firestore.collection('students').doc(docId).delete();
+  }
+
+  Future<String> _generateStudentId(String classID) async {
+    final counterRef = _firestore.collection('counters').doc(classID);
+    return _firestore.runTransaction((transaction) async {
+      final snapshot = await transaction.get(counterRef);
+      int nextIndex = 1;
+
+      if (snapshot.exists && snapshot.data()!.containsKey('nextIndex')) {
+        nextIndex = (snapshot.data()!['nextIndex'] as int) + 1;
+      }
+
+      transaction.set(counterRef, {'nextIndex': nextIndex});
+      return '$classID-${nextIndex.toString().padLeft(3, '0')}';
+    });
   }
 }
