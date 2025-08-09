1a0117cb0a6ba9778e0f54e0b859eb4637d6362e
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-08-10 01:44:24 +0530

    Add cloud_firestore dependency and implement AdminRepository for authentication

---
+++ admin/lib/repositories/admin_repository.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AdminRepository {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //Firebase Auth for admin
+  Future<User?> signIn(String email, String password) async {
+    final cred = await _auth.signInWithEmailAndPassword(email: email, password: password);
+    return cred.user;
+  }
+
+  Future<void> signOut() => _auth.signOut();
+  User? get currentUser => _auth.currentUser;
+
+  //Classes
+  
+}
