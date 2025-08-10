c3c84d73b711af18cb717041bf5e89789a588b9c
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-08-10 14:33:34 +0530

    Refactor AuthBloc to handle authentication events and states

--- client/lib/blocs/auth/auth_bloc.dart
+++ client/lib/blocs/auth/auth_bloc.dart
@@ -1,5 +1,39 @@
 import 'package:ama_meet/models/student.dart';
+import 'package:ama_meet/repositories/student_repository.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final StudentRepository repo;
+
+  AuthBloc(this.repo) : super(AuthInitial()) {
+
+    on<AppStarted>((event, emit) async {
+      emit(AuthLoading());
+      final student = await repo.getSavedStudent();
+      if (student != null) {
+        emit(AuthAuthenticated(student));
+      } else {
+        emit(AuthLoggedOut());
+      }
+    });
+
+    on<LoginRequested>((event, emit) async {
+      emit(AuthLoading());
+      final student = await repo.loginWithIdOrEmail(event.idOrEmail, event.password);
+      if (student != null) {
+        emit(AuthAuthenticated(student));
+      } else {
+        emit(AuthFailier("Invalid ID/Email or password"));
+      }
+    });
+
+    on<LogoutRequested>((event, emit) async {
+      await repo.logout();
+      emit(AuthLoggedOut());
+    });
+  }
+}
