4cb3844d1fbc2650ca2bf182a6713795155e83f1
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-08-10 13:10:51 +0530

    Add Student model implementation with serialization methods

---
+++ client/lib/models/student.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+
+class Student extends Equatable{
+  final String id;
+  final String classId;
+  final String name;
+  final String email;
+  final String passwordHash;
+  final int createdAt;
+
+  const Student({
+    required this.id,
+    required this.classId,
+    required this.name,
+    required this.email,
+    required this.passwordHash,
+    required this.createdAt,
+  });
+
+  factory Student.fromMap(Map<dynamic, dynamic> m) {
+    return Student(
+      id: m['id'] ?? '',
+      classId: m['classId'] ?? '',
+      name: m['name'] ?? '',
+      email: m['email']  ?? '',
+      passwordHash: m['passwordHash'] ?? '',
+      createdAt: m["createdAt"] ?? 0,
+    );
+  }
+
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'classId': classId,
+    'name': name,
+    'email': email,
+    'passwordHash': passwordHash,
+    'createdAt': createdAt,
+  };
+
+  @override
+  List<Object?> get props => [id, classId, name, email, passwordHash, createdAt];
+}
