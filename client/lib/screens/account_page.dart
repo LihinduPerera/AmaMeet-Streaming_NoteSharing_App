6c00e75de3aeb77d388e35a700e26e7df3d0fa59
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-08-10 15:13:07 +0530

    Refactor AccountPage to use Bloc for user authentication state management and remove unused code

--- client/lib/screens/account_page.dart
+++ client/lib/screens/account_page.dart
@@ -1,103 +1,60 @@
-import 'package:ama_meet/utils/colors.dart';
+import 'package:ama_meet/blocs/auth/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ama_meet/utils/colors.dart';
 
-class AccountPage extends StatefulWidget {
+class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
 
-  @override
-  State<AccountPage> createState() => _AccountPageState();
-}
-
-class _AccountPageState extends State<AccountPage> {
-
-  // final AuthService _authService = AuthService();
-  // UserModel? _userModel;
-  bool _isLoadng = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadUserData();
-  }
-
-  Future<void> _loadUserData() async {
-    // UserModel? user = await _authService.getUserData();
-    if(mounted) {
-      setState(() {
-        // _userModel = user;
-        _isLoadng = false;
-      });
-    }
-  }
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFeeedf2),
-
       appBar: AppBar(
         backgroundColor: const Color(0xFFeeedf2),
         elevation: 0,
         title: const Text("Your Account"),
         centerTitle: true,
       ),
-
-      body: _isLoadng
-        // ? const Center(
-        //   child: CircularProgressIndicator(),
-        // )
-        // : _userModel == null
-          ? const Center(
-            child: Text("No User Data Found !!")
-          )
-          :Center(
-            child: Padding(
+      body: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is AuthAuthenticated) {
+            final student = state.student;
+            return Padding(
               padding: const EdgeInsets.all(16),
               child: Column(
                 children: [
-                  // CircleAvatar(
-                  //   radius: 50,
-                  //   backgroundImage: NetworkImage(_userModel!.profilePhoto),
-                  // ),
-                  // const SizedBox(
-                  //   height: 16,
-                  // ),
-                  // Text(
-                  //   _userModel!.username,
-                  //   style: const TextStyle(
-                  //     fontSize: 22,
-                  //     fontWeight: FontWeight.bold
-                  //   ),
-                  // ),
-                  // const SizedBox(
-                  //   height: 10,
-                  // ),
-                  // Text(
-                  //   // _userModel!.uid,
-                  //   style: const TextStyle(
-                  //     fontSize: 16,
-                  //     color:  Colors.grey
-                  //   ),
-                  // ),
+                  const SizedBox(height: 20),
+                  Text(
+                    student.name,
+                    style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(student.email, style: const TextStyle(color: Colors.grey)),
                   const Spacer(),
                   ElevatedButton.icon(
-                    onPressed: () async{
-                      // await _authService.signOut();
-                      if(context.mounted) {
-                        Navigator.of(context).pushReplacementNamed('/login');
-                      }
+                    onPressed: () {
+                      context.read<AuthBloc>().add(LogoutRequested());
+                      Navigator.pushReplacementNamed(context, '/login');
                     },
-                    icon: const Icon(Icons.logout, color: Colors.white,),
-                    label: const Text("Sign Out", style: TextStyle(color: Colors.white),),
+                    icon: const Icon(Icons.logout, color: Colors.white),
+                    label: const Text("Sign Out", style: TextStyle(color: Colors.white)),
                     style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
                       backgroundColor: buttonColor,
-                    )
-                  )
+                    ),
+                  ),
                 ],
               ),
-            ),
-          )
+            );
+          } else {
+            return const Center(child: Text("No user data found"));
+          }
+        },
+      ),
     );
   }
 }
