d5f8c02d7dba5978ceac4b8cf8c4ce5b1c4361b8
Lihindu Perera <lihindu.indudunu.perera@gmail.com>
2025-08-18 10:06:28 +0530

    updated

---
+++ admin/lib/blocs/video/video_bloc.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'video_event.dart';
+part 'video_state.dart';
+
+class ClassVideosBloc extends Bloc<ClassVideosEvent, ClassVideosState> {
+  final ClassVideoRepository repository;
+
+  ClassVideosBloc(this.repository) : super(ClassVideosInitial()) {
+    on<LoadClassVideos>((event, emit) async {
+      emit(ClassVideosLoading());
+      try {
+        final videos = await repository.getVideos(event.classId);
+        emit(ClassVideosLoaded(videos));
+      } catch (e) {
+        emit(ClassVideosError(e.toString()));
+      }
+    });
+
+    on<UploadClassVideoEvent>((event, emit) async {
+      try {
+        await repository.uploadVideo(
+          event.classId,
+          event.file,
+          event.filename,
+          event.sectionTitle,
+          event.sectionOrder,
+        );
+      } catch (e) {
+        emit(ClassVideosError(e.toString()));
+      }
+    });
+
+    on<DeleteClassVideoEvent>((event, emit) async {
+      try {
+        await repository.deleteVideo(event.docId, event.publicId, event.localFilename);
+      } catch (e) {
+        emit(ClassVideosError(e.toString()));
+      }
+    });
+  }
+}
