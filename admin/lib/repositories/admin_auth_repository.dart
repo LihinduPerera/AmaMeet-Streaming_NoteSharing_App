29c23753dee10b273ef14429a31bfe1b535a8ed7
Lihindu Perera <lihindu.indudunu.perera@gmail.com>
2025-08-20 00:51:08 +0530

    updated

--- admin/lib/repositories/admin_auth_repository.dart
+++ admin/lib/repositories/admin_auth_repository.dart
@@ -1,17 +1,57 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AdminAuthRepository {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
 
-  //Firebase Auth for admin
   Future<User?> signIn(String email, String password) async {
-    final cred = await _auth.signInWithEmailAndPassword(email: email, password: password);
+    final cred = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
     return cred.user;
   }
 
-  Future<void> signOut() => _auth.signOut();
+  Future<User?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? gUser = await _googleSignIn.authenticate();
+      if (gUser == null) return null;
+
+      final GoogleSignInAuthentication gAuth = await gUser.authentication;
+      final String? idToken = gAuth.idToken;
+
+      if (idToken == null) {
+        throw Exception("Failed to retrieve idToken from GoogleSignIn");
+      }
+
+      final credential = GoogleAuthProvider.credential(idToken: idToken);
+
+      final userCred = await _auth.signInWithCredential(credential);
+      final user = userCred.user;
+
+      if (user != null) {
+        await _firestore.collection('admins').doc(user.uid).set({
+          'email': user.email,
+          'displayName': user.displayName,
+          'lastSignIn': FieldValue.serverTimestamp(),
+        }, SetOptions(merge: true));
+      }
+
+      return user;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _googleSignIn.disconnect();
+    } catch (_) {}
+    await _auth.signOut();
+  }
+
   User? get currentUser => _auth.currentUser;
-  
 }
